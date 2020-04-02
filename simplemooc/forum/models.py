cae20cf3ae79917ecf16418cf2378f0f294ea2ea
Aubay Aml <amleite@aubay.com>
2020-04-02 16:30:46 +0100

    Changed forum to support adding replies

--- simplemooc/forum/models.py
+++ simplemooc/forum/models.py
@@ -3,11 +3,14 @@ from django.conf import settings
 
 from taggit.managers import TaggableManager
 
+
 class Thread(models.Model):
-    title = models.CharField('Title', max_length=100)
+
+    title = models.CharField('Titlte', max_length=100)
+    slug = models.SlugField('Slug', max_length=100, unique=True)
     body = models.TextField('Message')
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, models.DO_NOTHING, verbose_name='Author', related_name='threads'
+        settings.AUTH_USER_MODEL, models.DO_NOTHING,verbose_name='Autor', related_name='threads'
     )
     views = models.IntegerField('Views', blank=True, default=0)
     answers = models.IntegerField('Answers', blank=True, default=0)
@@ -15,11 +18,13 @@ class Thread(models.Model):
     tags = TaggableManager()
 
     created = models.DateTimeField('Created at', auto_now_add=True)
-    modified = models.DateTimeField('Modified at', auto_now=True)
+    modified = models.DateTimeField('Updated at', auto_now=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return "/forum/%s" % self.slug
 
     class Meta:
         verbose_name = 'Topic'
@@ -28,12 +33,14 @@ class Thread(models.Model):
 
 
 class Reply(models.Model):
+
     thread = models.ForeignKey(
         Thread, models.DO_NOTHING, verbose_name='Topic', related_name='replies'
     )
     reply = models.TextField('Reply')
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, models.DO_NOTHING, verbose_name='Author', related_name='replies'
+        settings.AUTH_USER_MODEL, models.DO_NOTHING, verbose_name='Autor', 
+        related_name='replies'
     )
     correct = models.BooleanField('Correct?', blank=True, default=False)
 
@@ -43,7 +50,28 @@ class Reply(models.Model):
     def __str__(self):
         return self.reply
 
+
     class Meta:
-        verbose_name = 'Reply'
-        verbose_name_plural = 'Replies'
+        verbose_name = 'Resposta'
+        verbose_name_plural = 'Respostas'
         ordering = ['-correct', 'created']
+
+
+def post_save_reply(created, instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+    if instance.correct:
+        instance.thread.replies.exclude(pk=instance.pk).update(
+            correct=False
+        )
+
+def post_delete_reply(instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+models.signals.post_save.connect(
+    post_save_reply, sender=Reply, dispatch_uid='post_save_reply'
+)
+models.signals.post_delete.connect(
+    post_delete_reply, sender=Reply, dispatch_uid='post_delete_reply'
+)
--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tag/<tag>/', views.index, name='index_tagged'),
+    path('replies/<pk>/correct/', views.reply_correct, name='reply_correct'),
+    path('respostas/<pk>/incorrect/', views.reply_incorrect, name='reply_incorrect'),
+    path('<slug:slug>/', views.thread, name='thread'),
 ]
